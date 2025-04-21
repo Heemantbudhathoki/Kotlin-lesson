7c57415ce5b4d732f160a4eea8c9fed3e41ffbb2
Heemantbudhathoki <heemantbudhathoki2@gmail.com>
2025-04-21 10:10:23 +0545

    Initial commit

---
+++ app/src/main/java/com/example/project00/ui/theme/heemant.kt
@@ -0,0 +1,90 @@
+//package com.example.project00.ui.theme
+//
+//import java.sql.DriverManager.println
+//import java.util.ArrayList
+//
+//fun main() {
+//    println("hello world")
+//
+//
+//    // Properly define and use the array
+//    val addressArray = arrayOf("ktm", "address", "bhaktapur")
+//    // Arrays are fixed in size; to add a new element, use a list instead
+//    println(addressArray.joinToString())
+//
+//    var name: String = "Heemant"
+//    val age: Int = 18
+//
+//    // Use ArrayList for dynamic collections
+//    val addressList = arrayListOf("ktm", "chitwan")
+//    addressList.add("pokhara")
+//
+//    println("Name: $name")
+//    println("Age: $age")
+//    println("Address List: $addressList")
+//
+//    // Declare an integer ArrayList
+//    val data = ArrayList<Int>()
+//    data.add(10)
+//    data.add(20)
+//
+//    println("Data: $data")
+//
+//
+//    val dataa= ArrayList<Any>()  // it takes all string, int ....
+//
+////    mutable list and imutable list
+//
+//
+//
+//
+//    val capitalCities = mapOf(
+//        "Nepal" to "Kathmandu",
+//        "India" to "New Delhi",
+//        "USA" to "Washington, D.C.",
+//        "UK" to "London"
+//    )
+//
+//    // Accessing value using key
+//    println("Capital of Nepal: ${capitalCities["Nepal"]}")
+//
+//    // Loop through map
+//    for ((country, capital) in capitalCities) {
+//        println("$country -> $capital")
+//    }
+//
+//
+//    val name = "Hemanta"
+//    println("Length of the string is: ${name.length}")
+//
+//
+//
+//        val day = 3
+//
+//        when (day) {
+//            1 -> println("Sunday")
+//            2 -> println("Monday")
+//            3 -> println("Tuesday")
+//            4 -> println("Wednesday")
+//            5 -> println("Thursday")
+//            6 -> println("Friday")
+//            7 -> println("Saturday")
+//            else -> println("Invalid day")
+//        }
+//
+//
+//    for (i in 1..10 step 2) {
+//        println("Step by 2: $i")
+//    }
+//
+//
+//    public int calculator(int a, int b){
+//
+//    }
+//
+//
+//    fun calculator(a:int,b:int):Unit{
+//
+//
+//    }
+//}
---
+++ app/src/main/java/com/example/project00/week1.kt
@@ -0,0 +1,6 @@
+package com.example.project00
+
+
+fun main() {
+    var address = arrayOf("thisi is the name")
+}
